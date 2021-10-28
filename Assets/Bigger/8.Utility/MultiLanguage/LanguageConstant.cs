22b754b4285b4d58e212d3538dde8c25d9169967
愤怒的阿凡提 <1413698201@qq.com>
2021-10-28 11:14:20 +0800

    多语言生成支持多个对象同时选中

--- Assets/Bigger/8.Utility/MultiLanguage/LanguageConstant.cs
+++ Assets/Bigger/8.Utility/MultiLanguage/LanguageConstant.cs
@@ -3,13 +3,10 @@ namespace Bigger
 {
     public static class LanguageConstant
     {
-        public const string 哟哟 = "e33eb83044bbca19705dbe31d4c13da3";
-		public const string 切克闹 = "830b11b1804947100ac081f62ef23198";
-		public const string 游戏 = "ba08216f13dd1742157412386eee1225";
-		public const string 确定 = "38cf16f2204ffab8a6e0187070558721";
-		public const string 取消 = "625fb26b4b3340f7872b411f401e754c";
-		public const string 确___定 = "cb4e0070adb3beb98519e83566a5d088";
-		public const string 拉拉拉拉啊啦啦啦拉拉拉拉啊啦啦啦 = "d7bbfa9d624335271025d7fd74c708ae";
+        public const string 游戏 = "ba08216f13dd1742157412386eee1225";
+		public const string Unity = "82dd98f1d663acc318e6a8ec4ab69b7a";
+		public const string 游戏2 = "be5332955b7f02e5cad1c257ed3c4696";
+		public const string Unity2 = "41b1031d2e94a8033205fa22b4a3ee0e";
 		
     }
 }
--- Assets/Bigger/Editor/AutoCreateScript/MultiLanguageTool.cs
+++ Assets/Bigger/Editor/AutoCreateScript/MultiLanguageTool.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,15 +8,19 @@ namespace Bigger
 {
     public class MultiLanguageTool
     {
-
         [MenuItem("Bigger/自动生成/当前对象的多语言")]
         static void ExcuteLanguageUpdate()
         {
             if (Selection.activeGameObject == null) return;
-            LanguageText[] languageTexts = Selection.activeGameObject.GetComponentsInChildren<LanguageText>(true);
-            if (languageTexts != null && languageTexts.Length > 0)
+            List<LanguageText> languageTexts = new List<LanguageText>();
+            foreach (var ui in Selection.gameObjects)
             {
-                var languageDict =ReadEditorLanguageJson();
+                LanguageText[] itemTexts = ui.GetComponentsInChildren<LanguageText>(true);
+                languageTexts.AddRange(itemTexts);
+            }
+            if (languageTexts != null && languageTexts.Count > 0)
+            {
+                var languageDict = ReadEditorLanguageJson();
                 foreach (var item in languageTexts)
                 {
                     AddNewLanguageText(item.InitKey(), item.GetDefaultStr(), languageDict);
@@ -66,7 +71,7 @@ namespace Bigger
             var dict = ReadEditorLanguageJson();
             foreach (var item in dict)
             {
-                var += $"public const string {item.Value.Chinese.ToAlphaNumberAndChinese(false).Replace(" ","_")} = \"{item.Key}\";\r\n\t\t";
+                var += $"public const string {item.Value.Chinese.ToAlphaNumberAndChinese(false).Replace(" ", "_")} = \"{item.Key}\";\r\n\t\t";
             }
             classStr = classStr.Replace("$变量", var);
             File.WriteAllText($"{Application.dataPath}/Bigger/8.Utility/MultiLanguage/LanguageConstant.cs", classStr);
